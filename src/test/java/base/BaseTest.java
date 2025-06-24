4fb11191f05b3a4f6dea088a1bcd997cf1c95a1d
jihad <jihad.owais@t3.com.eg>
2025-06-24 19:26:25 +0300

    Add verifyElementsAreClickableTest using List<WebElement> & driver.findElements - Add isValidURL() a util helpful method

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -6,9 +6,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import org.testng.annotations.*;
-
 import org.testng.asserts.SoftAssert;
 import org.testng.Assert;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
 
 public class BaseTest {
     private WebDriver driver;
@@ -17,6 +21,7 @@ public class BaseTest {
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver");
         driver = new ChromeDriver();
+        driver.manage().window().minimize();
     }
 
     @AfterClass
@@ -29,7 +34,7 @@ public class BaseTest {
         driver.get("https://the-internet.herokuapp.com/");
     }
 
-    @Test(enabled = false, dataProvider = "getPageTitle", dataProviderClass = TestData.class)
+    @Test(dataProvider = "getPageTitle", dataProviderClass = TestData.class)
     public void shouldShowAppTitleCorrectlyTest(String page, String title) {
         String actual = driver.getTitle();
         Assert.assertEquals(actual, title, "Page has no or wrong title .");
@@ -47,5 +52,31 @@ public class BaseTest {
         softAssert.assertEquals(actualURL, expectedURL, "Wrong redirection URL.");
 
         softAssert.assertAll("Some elements are not visible or have wrong redirection URL.");
+    }
+
+    @Test
+    public void verifyElementsAreClickableTest() {
+        List<WebElement> webElements = driver.findElements(By.tagName("li"));
+        SoftAssert softAssert = new SoftAssert();
+
+        for (WebElement element:webElements) {
+            String elementName = element.getText();
+            String elementURL = element.findElement(By.tagName("a")).getDomProperty("href");
+            System.out.println("Element: " + elementName + ", Link: " + elementURL);
+            softAssert.assertTrue(isValidURL(elementURL), "Element is not clickable, may be the URL is wrong or not found. The found element URL is: " + elementURL);
+        }
+
+        softAssert.assertAll();
+    }
+
+    boolean isValidURL(String url) {
+        try {
+            new URL(url).toURI();
+            return true;
+        } catch (MalformedURLException e) {
+            return false;
+        } catch (URISyntaxException e) {
+            return false;
+        }
     }
 }
