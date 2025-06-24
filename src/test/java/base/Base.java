38bd90f59665d6a24d7db3c5331d612e6eb06e74
jihad <jihad.owais@t3.com.eg>
2025-06-24 12:52:08 +0300

    Change window size using [.maximize(), .fullscreen(), .setSize(new Dimension(w, l))]

--- src/test/java/base/Base.java
+++ src/test/java/base/Base.java
@@ -1,5 +1,6 @@
 package base;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
@@ -16,11 +17,13 @@ public class Base {
         // instantiate a driver object to interact with the browser & do any automations
         driver = new ChromeDriver(); // we can instantiate any other driver // ex. FirefoxDriver(); EdgeDriver(); ..
         driver.get("https://the-internet.herokuapp.com/"); // launch the browser for our App
+        driver.manage().window().maximize(); // maximize the window // .fullscreen() to make it in fullscreen mode
+        driver.manage().window().setSize(new Dimension(375, 812)); // set window size to iphone size
     }
 
     @Test
     public void verifyAppTitleTest() {
         System.out.println(driver.getTitle()); // print the App title
-        driver.close();
+        driver.quit();  // close(): closes the window, NOT the sessions // quit(): close any of the windows that are open and complete out this session.
     }
 }
