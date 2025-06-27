028c1c694d23d24ce5f923dd3f73a7f78c6ccdca
jihad <jihad.owais@t3.com.eg>
2025-06-27 18:47:35 +0300

    Handle internal server error 500 if Server did not redirect to Email Sent page (To avoid 'NoSuchElementException' exception, instead, make the test method fails

--- src/main/java/pages/EmailSentPage.java
+++ src/main/java/pages/EmailSentPage.java
@@ -3,6 +3,8 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.util.Objects;
+
 public class EmailSentPage {
     private final WebDriver driver;
     private final By messageContent = By.id("content");
@@ -16,5 +18,9 @@ public class EmailSentPage {
     public boolean isEmailSent() {
         String url = "https://the-internet.herokuapp.com/email_sent";
         return url.equalsIgnoreCase(driver.getCurrentUrl());
+    }
+
+    public boolean isOnEmailSentPage() {
+        return Objects.requireNonNull(driver.getCurrentUrl()).contains("/email_sent");
     }
 }
--- src/test/java/quiz/chapter4/ForgotPasswordTests.java
+++ src/test/java/quiz/chapter4/ForgotPasswordTests.java
@@ -1,6 +1,7 @@
 package quiz.chapter4;
 
 import base.BaseTests;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pages.EmailSentPage;
@@ -11,6 +12,9 @@ public class ForgotPasswordTests extends BaseTests {
     public void validateForgotPasswordSendEmail(boolean expectedIsEmailSent, String email, String expectedAlertMessage, String assertionSentEmailErrorMessage) {
         ForgotPasswordPage forgetPasswordPage = homePage.clickForgetPassword();
         EmailSentPage emailSentPage = forgetPasswordPage.retrievePassword(email);
+
+        // handle internal server error 500
+        Assert.assertTrue(emailSentPage.isOnEmailSentPage(), "Server did not redirect to Email Sent page. Possible 500 error.");
 
         boolean actualIsEmailSent = emailSentPage.isEmailSent(); // should be enhanced to cover the non-sent-email scenario but the server now is down: "internal server error" with status-code: 500
         String actualAlertMessage = emailSentPage.getMessageContent();
