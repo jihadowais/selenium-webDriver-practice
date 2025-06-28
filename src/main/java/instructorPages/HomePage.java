ac9f677048eb48e33253774e5461da9b60a737aa
jihad <jihad.owais@t3.com.eg>
2025-06-28 11:54:17 +0300

    Add Hovers Page functionality (Instructor's Solution)

---
+++ src/main/java/instructorPages/HomePage.java
@@ -0,0 +1,22 @@
+package instructorPages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+    private WebDriver driver;
+
+    public HomePage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public HoversPage clickHovers(){
+        clickLink("Hovers");
+        return new HoversPage(driver);
+    }
+
+    private void clickLink(String linkText){
+        driver.findElement(By.linkText(linkText)).click();
+    }
+}
