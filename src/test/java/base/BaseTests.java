b08d0dd57c9c4f996b4897a72dafc20157237930
jihad <jihad.owais@t3.com.eg>
2025-06-28 23:09:18 +0300

    Add take screenshots on failures functionality (Add @AfterMethod: recordFailure(ITestResult result) to BaseTests class)

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,10 +1,17 @@
 package base;
 
+import com.google.common.io.Files;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 
 import pages.HomePage;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BaseTests {
     private WebDriver driver;
@@ -27,5 +34,19 @@ public class BaseTests {
     public void openHomePage() {
         driver.get(domainURL);
         homePage = new HomePage(driver);
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result){
+        if(ITestResult.FAILURE == result.getStatus())
+        {
+            var camera = (TakesScreenshot)driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try{
+                Files.move(screenshot, new File("resources/screenshots/" + result.getName() + ".png"));
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
