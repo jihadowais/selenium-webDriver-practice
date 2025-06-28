f6c6b7c48685b61ef83a4300d9827a4e09dd5b8c
jihad <jihad.owais@t3.com.eg>
2025-06-28 11:40:02 +0300

    Remove unwanted empty line from HoversPage.java file

--- src/main/java/pages/HoversPage.java
+++ src/main/java/pages/HoversPage.java
@@ -11,7 +11,6 @@ public class HoversPage {
     private final WebDriver driver;
     private By figure;
 
-
     public HoversPage(WebDriver driver) { this.driver = driver; }
 
     private void setFigure(String userName) {
